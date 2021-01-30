3d13060b8a63fdd7a744705873270aa74407d469
Timur <timur@anonshop.ru>
2021-01-30 14:42:26 +0300

    решена задача 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+function insertValueToSelector($selector, $value, $operator = "add more"){
+    if ($operator == "equal") document.querySelector($selector).innerHTML = $value;
+    if ($operator == "add more") document.querySelector($selector).innerHTML += $value;
+}
+
 function getTemp(){
-    // Переменные
+    // Объявляем переменные
     let input = document.querySelector('.first-task-input'),
         celsium,
         fahrenheit;
@@ -23,4 +28,20 @@ function getTemp(){
 
     // Выводим результат пользователю
     alert("Указанная температура в градусах по Фаренгейту: "+ fahrenheit +" градусов");
+}
+
+function getNameAdmin(){
+    let admin,
+        name;
+    name = "Василий";
+    admin = name;
+
+    setTimeout(() => {  insertValueToSelector(".second-task-result", "<span class=\"code\">var</span> <span class=\"variable\">admin</span>, <span class=\"variable\">name</span>; // <span class=\"comment\">Объявляем переменные</span><br>", "equal"); }, 700);
+    setTimeout(() => {  insertValueToSelector(".second-task-result", "<span class=\"variable\">name</span> = \"Василий\"; // <span class=\"comment\">Вносим значение переменной name</span><br>"); }, 1400);
+    setTimeout(() => {  insertValueToSelector(".second-task-result", "<span class=\"variable\">admin</span> = <span class=\"variable\">name</span>; // <span class=\"comment\">Присваиваем значение переменной name переменной admin</span><br>"); }, 2100);
+    setTimeout(() => {  insertValueToSelector(".second-task-result", "<span class=\"code\">alert(</span><span class=\"variable\">admin</span><span class=\"code\">)</span>; // <span class=\"comment\">Выводим результат</span><br>"); }, 2800);
+    setTimeout(() => {  
+        alert(admin); 
+        document.querySelector('.content-section-button').disabled = false;
+    }, 3500);
 }
