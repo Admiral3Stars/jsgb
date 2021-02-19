b7820a0353d187e965295cc0831e72e57f6fb09b
Timur <timur@anonshop.ru>
2021-02-19 13:47:07 +0300

    Решена задача 2 урока 6

--- lesson-6.php
+++ lesson-6.php
@@ -25,7 +25,33 @@
         <section class="section-tasks">
             <p class="content-text">Задача #2. Реализовать модуль корзины. Создать блок товаров и&nbsp;блок корзины. У&nbsp;каждого товара есть кнопка &laquo;Купить&raquo;, при нажатии на&nbsp;которую происходит добавление имени и&nbsp;цены товара в&nbsp;блок корзины. Корзина должна уметь считать общую сумму заказа.</p>
             <form class="content-form form-center-button" action="javascript:void(0)">
-                <button class="content-form-button" onclick="">Выполнить</button></p>
+            <div class="content-cart"></div>
+            <div class="content-item">
+                <div class="item-box" data-id="1">
+                    <img src="images/televizor.jpg" alt="Телевизор" class="item-box-image" width="150px" height="150px">
+                    <span class="item-box-text">Телевизор</span>
+                    <span class="item-box-price" data-price="17999.00">17999.00 руб.</span>
+                    <span class="item-box-quantity-text">введите кол-во:</span>
+                    <input type="number" class="item-box-quantity content-form-number" step="1">
+                    <button class="shop content-form-button long-button">Купить</button>
+                </div>
+                <div class="item-box" data-id="2">
+                    <img src="images/smatphone.jpg" alt="Смартфон" class="item-box-image" width="150px" height="150px">
+                    <span class="item-box-text">Смартфон</span>
+                    <span class="item-box-price" data-price="8641.00">8641.00 руб.</span>
+                    <span class="item-box-quantity-text">введите кол-во:</span>
+                    <input type="number" class="item-box-quantity content-form-number" step="1">
+                    <button class="shop content-form-button long-button">Купить</button>
+                </div>
+                <div class="item-box" data-id="3">
+                    <img src="images/beer.jpg" alt="Пиво" class="item-box-image" width="150px" height="150px">
+                    <span class="item-box-text">Пиво</span>
+                    <span class="item-box-price" data-price="45.07">45.07 руб.</span>
+                    <span class="item-box-quantity-text">введите кол-во:</span>
+                    <input type="number" class="item-box-quantity content-form-number" step="1">
+                    <button class="shop content-form-button long-button">Купить</button>
+                </div>
+            </div>
             </form>
         </section>
         <section class="section-tasks">
